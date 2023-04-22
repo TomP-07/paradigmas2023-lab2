7f66ea409774f7a5a43ce6142224ac09ecd93664
Tomás Ponce Gessi <tomaspgwk@gmail.com>
2023-04-22 01:54:37 -0300

    Codigo inicial del HTTPRequester

--- src/HTTPRequest/HTTPRequester.java
+++ src/HTTPRequest/HTTPRequester.java
@@ -6,16 +6,44 @@ package HTTPRequest;
  * https://www.baeldung.com/java-http-request
  * */
 
-public class HTTPRequester {
-	
-	public String getFeedRss(String urlFeed){
-		String feedRssXml = null;
-		return feedRssXml;
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-	public String getFeedReedit(String urlFeed) {
-		String feedReeditJson = null;
-		return feedReeditJson;
-	}
+public class HTTPRequester {
+
+    public HTTPRequester() {
+
+    }
+
+    public String getFeedRss(String urlFeed) throws MalformedURLException, IOException {
+        URL url = new URL(urlFeed);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = connection.getResponseCode();
+        System.out.printf("Response code: %s%n", responseCode);
+
+        InputStream responseStream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = reader.readLine()) != null) response.append(inputLine);
+        reader.close();
+        responseStream.close();
+
+        String feedRssXml = response.toString();
+        return feedRssXml;
+    }
+
+    public String getFeedReedit(String urlFeed) {
+        String feedReeditJson = null;
+        return feedReeditJson;
+    }
 
 }
