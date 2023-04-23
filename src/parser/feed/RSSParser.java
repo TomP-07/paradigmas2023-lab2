8c132abb61418438b9997305ca35703272246ec1
MolinaFranco <franco13mm@gmail.com>
2023-04-22 22:56:42 -0300

    primera estructura de parser RSS

--- src/parser/feed/RSSParser.java
+++ src/parser/feed/RSSParser.java
@@ -1,5 +1,6 @@
 package parser.feed;
 
+import feed.Article;
 import feed.Feed;
 import org.w3c.dom.*;
 import org.xml.sax.InputSource;
@@ -7,6 +8,12 @@ import parser.feed.FeedParser;
 
 import javax.xml.parsers.*;
 import java.io.*;
+import java.lang.reflect.Array;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /* Esta clase implementa el parser de feed de tipo rss (xml)
  * https://www.tutorialspoint.com/java_xml/java_dom_parse_document.htm
@@ -17,18 +24,44 @@ public class RSSParser extends FeedParser {
     @Override
     public Feed parseFeed(String rawRSS) {
         try {
+            
             DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
+
             Document xml = builder.parse(new InputSource(new StringReader(rawRSS)));
             xml.getDocumentElement().normalize();
             System.out.println("Root ELEM: " + xml.getDocumentElement().getNodeName());
+            // consigo la lista de items de la pagina
+            NodeList itemNodes = xml.getElementsByTagName("item");
+
+            // genero la lista de titulos para poder crear el feed con su determinado titulo
             NodeList titles = xml.getElementsByTagName("title");
-            for (int i = 0; i < titles.getLength(); i++) {
-                Node title = titles.item(i);
-                if (title.getNodeType() != Node.ELEMENT_NODE)
-                    continue;
-                System.out.println(title.getTextContent());
+
+            // genero el feed que retornaremos
+            Feed feed = new Feed(titles.item(0).toString());
+
+            // por cada item consigo su titulo, link, descripcion y fecha
+            for (int i = 0; i < itemNodes.getLength(); i++) {
+
+                Node itemNode = itemNodes.item(i);
+                if (itemNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element itemElement = (Element) itemNode;
+                    String title = itemElement.getElementsByTagName("title").item(0).getTextContent();
+                    String link = itemElement.getElementsByTagName("link").item(0).getTextContent();
+                    String description = itemElement.getElementsByTagName("description").item(0).getTextContent();
+                    String pubDateS = itemElement.getElementsByTagName("pubDate").item(0).getTextContent();
+                    
+                    // formateo la fecha
+                    String inputFormat = "EEE, dd MMM yyyy HH:mm:ss Z";
+                    SimpleDateFormat inputDateFormat = new SimpleDateFormat(inputFormat, Locale.ENGLISH);
+                    Date pubDate = (Date) inputDateFormat.parse(pubDateS);
+                    
+                    // añado el articulo al feed
+                    Article newArticle = new Article(title,description,pubDate,link);
+                    feed.addArticle(newArticle);
+                }                
             }
+
         } catch (ParserConfigurationException e) {
 
         } catch (Exception e) {
