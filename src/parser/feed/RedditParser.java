0e3586c116ac4f9059fd6336feaf437ed80c2d59
vicky <victorialopez23@mi.unc.edu.ar>
2023-05-05 10:27:15 -0300

    parser reddit

--- src/parser/feed/RedditParser.java
+++ src/parser/feed/RedditParser.java
@@ -1,21 +1,80 @@
 package parser.feed;
 
 
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import feed.Article;
+
 /*
  * Esta clase implementa el parser de feed de tipo reddit (json)
  * pero no es necesario su implemntacion
  * */
 
 import feed.Feed;
-import parser.feed.FeedParser;
-
-import java.io.StringReader;
 
 public class RedditParser extends FeedParser {
 
-    public Feed parseFeed(String rawReddit) {
-        StringReader reader = new StringReader(rawReddit);
+    // private String title;
+
+    // // constructor inecesario ya que encontraos campo subreddit jeje
+    // public RedditParser(String title){
+    //     this.title = title;
+    // }
+
+    @Override
+    public Feed parseFeed(String rawReddit){
+        try {
+
+            //creo un objeto json con el rawReddit
+            JSONObject obj = new JSONObject(rawReddit);
+            JSONObject data = obj.getJSONObject("data");  
+            JSONArray children = data.getJSONArray("children");
+            JSONObject data2 = children.getJSONObject(0).getJSONObject("data");
+            //OBTENEMOS TITULO como reddit no tiene titulo colocamos el subreddit para guiarnos
+            String title = data2.getString("subreddit");
+           
+            //CREAR FEED
+            Feed feed = new Feed(title);
+          
+
+            //recorrer los children
+               //obtengo el titulo,descripcion,fecha,link de cada articulo
+            for (int i = 0; i < children.length(); i++) {
+                JSONObject child = children.getJSONObject(i);
+                JSONObject dataChild = child.getJSONObject("data");
+                String titulo = dataChild.getString("title");
+                String description = dataChild.getString("selftext");
+                double pubDate = dataChild.getDouble("created_utc");
+                String link = dataChild.getString("url");
+
+                //formateo la fecha
+                // Convertir la fecha de formato flotante a Date
+                Date date = new Date((long) (pubDate * 1000));
+        
+               // añado el articulo al feed
+                Article newArticle = new Article(titulo,description,date,link);
+                feed.addArticle(newArticle);
+                }
+
+            return feed;
+        } catch (JSONException e) {
+            System.out.printf("Error en el parseo de JSON: %s%n", e);
+        } catch(Exception e){
+            System.out.printf("Error en el parseo: %s%n", e);
+        }
+       
         return null;
     }
 
 }
+
+
+        
+
+
+
+
